26ece73708222317f2c9888f9fabe30a5e64accc
Jayesh Lata <Jayesh.Lata@makemytrip.com>
2016-12-29 02:29:57 +0530

    added chat service

---
+++ app/src/main/java/com/baapka/bol/ChatService.java
@@ -0,0 +1,13 @@
+package com.baapka.bol;
+
+/**
+ * Created by jayesh on 12/28/2016.
+ */
+public class ChatService {
+
+    private static final String TAG = "ChatService";
+
+    public ChatService() {
+        // load config here
+    }
+}
