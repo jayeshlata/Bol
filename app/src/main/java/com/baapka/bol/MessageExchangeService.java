be5df5ed2a0cb8eed081952048baf846d3159304
Jayesh Lata <Jayesh.Lata@go-mmt.com>
2017-08-20 11:12:54 +0530

    minor fixes

---
+++ app/src/main/java/com/baapka/bol/MessageExchangeService.java
@@ -0,0 +1,14 @@
+package com.baapka.bol;
+
+/**
+ * Created by jayesh on 21/5/17.
+ */
+
+public class MessageExchangeService {
+
+    private static String server_address = "localhost/";
+
+    public String getReplyFromServer() {
+        return null;
+    }
+}
